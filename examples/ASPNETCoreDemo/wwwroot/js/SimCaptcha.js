4ebc5a4773b70525be842e8d84fbca14dabcd024
yiyun <yiyungent@126.com>
2020-07-11 11:36:01 +0800

    Updated: SimCaptcha 框架更新

--- examples/ASPNETCoreDemo/wwwroot/js/SimCaptcha.js
+++ examples/ASPNETCoreDemo/wwwroot/js/SimCaptcha.js
@@ -1,19 +1,40 @@
 ﻿(function () {
 	// 私有字段
+	// 点击此 _element 元素则弹出验证码层，为此元素绑定点击事件
 	var _element = null;
 	var _appId = "";
 	// 前端验证成功后，会调用业务传入的回调函数，并在第一个参数中传入回调结果
 	var _callback = null;
-	var _options = null;
+	var _options = null; // 保留，暂时无用
 
-	var _vcodePos = [];
+	// 用户点击验证码图片的位置数据 {Array} eg: ["12,35", "134,656", "133,44", "33,13"]
+	var _vCodePos = [];
+
+	// 验证码服务端 效验url
+	var _reqVCodeCheckUrl = "";
+	// 验证码服务端 获取验证码图片url
+	var _reqVCodeImgUrl = "";
+
+	// 从后端响应得到的appId
+	var _resAppId = "";
+	// 验证是否通过效验票据
+	var _resTicket = "";
+	// 验证码图片 base64
+	var _resVCodeImg = "";
+	// 验证码效验秘钥
+	var _resVCodeKey = "";
+	// 验证码提示 eg: 请依序点击 走 炮 跳
+	var _resVCodeTip = "";
+
+	// 错误提示 eg: 1.点错啦，请重试 2.这题有点难，为你换一个试试吧
+	var _errorTip = "";
 
 	/***
 	 * 隐藏当前验证码弹出层，下次show 将使用当前验证码图片base64
 	 * 用于用户手动点击关闭按钮
 	 */
 	function hidden() {
-
+		// TODO: 需DOM操作
 	}
 
 	/***
@@ -22,31 +43,112 @@
 	function destroy() {
 		// 隐藏当前验证码弹出层
 		hidden();
-		// TODO: 清除验证码图片base64数据
+		// 清除全部点触标记
+		clearPointMark();
+		// 清空点触位置数据
+		_vCodePos = [];
+		// 清除验证码相关数据
+		_resVCodeImg = "";
+		_resVCodeKey = "";
+		_resVCodeTip = "";
+		_resAppId = "";
+		_resTicket = "";
+
+		_errorTip = "";
+	}
+
+
+
+	/**
+	 * 在验证码弹出层展示 成功通过验证
+	 * @param {Number} ts 本次点击验证码花费时间（js 13位时间戳）// 保留，暂时不用，随便传一个，或不传
+	 */
+	function showSuccessTip(ts) {
+		// TODO: 在验证码弹出层展示 成功通过验证
+		// TODO: 需DOM操作
+	}
+
+	/**
+	 * 清空图片上的全部点触标记
+	 */
+	function clearPointMark() {
+		// TODO: 找到 .simCaptcha 内的 所有 .simCaptcha_mark 元素 -> 全部移除
+		// TODO: 需DOM操作
 	}
 
 	/***
 	 * 将用户点击验证码的位置数据发送到验证码服务端   (每个位置(x轴, y轴))
-	 * @param posArr [Array] eg: ["12,35", "134,656", "133,44", "33,13"]
-	 * @return
+	 * @param posArr {Array} eg: ["12,35", "134,656", "133,44", "33,13"]
 	 */
 	function sendVCodePos(posArr) {
-		// TODO: 发送ajax到验证码服务端 -> 得到response结果，封装为 res
-		// 通过验证 -> 1.回调callback（成功回调） 2.销毁验证码弹出层destroy
-		var res = {ret:0, ticket:"143jghiafbwfb8143481nfdbf", appId:"133244", bizState:null};
-		_callback(res);
-		destroy();
+		var ts = Date.now(); // js 13位 毫秒时间戳
+		var userVerifyInfo = { vcodePos: posArr, ua: navigator.userAgent, ts: ts }; // ua, ts 服务端暂时未用，保留。用户花费在此验证码的时间 = 验证码服务端 接收到点击位置数据时间 - 验证码服务端 产生验证码图片时间
+		// 发送ajax到验证码服务端 -> 得到response结果，封装为 res
+		httpPost(_reqVCodeCheckUrl, userVerifyInfo, function (response) {
 
-		// 未通过验证 -> 1.提示用户 2.刷新验证码弹出层（请求新验证码图片，更新验证码提示信息）
-		
+			console.log("sendVCodePos", response);
+
+			// code: 0: 通过验证
+			if (response.code == 0) {
+				// 通过验证 -> 1.回调callback（成功回调） 2.销毁验证码弹出层destroy
+				var res = { ret: 0, ticket: response.data.ticket, appId: response.data.appId, bizState: null }; // bizState自定义透传参数，暂未实现，保留
+				// 将 从验证码服务端得到的 appId, ticket存起来
+				_resAppId = res.appId;
+				_resTicket = res.ticket;
+				_callback(res);
+				// 在摧毁验证码层之前，先在验证码层展示成功通过验证提示
+				showSuccessTip();
+				destroy();
+			} else {
+				// 未通过验证 -> 1.提示用户 2.if(错误次数未达上限)：清空用户点击验证码的位置数据，重置，让用户重新点击 3.else(错误次数达到上限)：刷新验证码弹出层（请求新验证码图片，更新验证码提示）
+				// code: -1: 验证码错误 且 错误次数未达上限
+				if (code == -1) {
+					_errorTip = "点错啦，请重试";
+					// 清空点触位置数据
+					_vCodePos = [];
+					// 清除图片上的全部点触标记
+					clearPointMark();
+				} else if (code == -2) {
+					// code: -2: 验证码错误 且 错误次数已达上限
+					_errorTip = "这题有点难，为你换一个试试吧";
+					refreshVCode();
+				}
+
+			}
+
+
+		});
+
+
+	}
+
+	/***
+	 * 刷新验证码弹出层：1.刷新验证码图片，2.更新验证码提示
+	 */
+	function refreshVCode() {
+		// ajax请求新的验证码图片base64
+		httpGet(_reqVCodeImgUrl, function (response) {
+			if (response.code == 0) {
+				// 成功获取新验证码
+				// 保存并更新 验证码图片
+				_resVCodeImg = response.data.vCodeImg;
+				// 保存验证码效验秘钥
+				_resVCodeKey = response.data.vCodeKey;
+				// 更新验证码提示
+				_resVCodeTip = response.data.vCodeTip;
+			} else {
+				// 获取验证码失败
+				_errorTip = response.message;
+			}
+		});
 	}
 
 	/***
 	 * 手动初始化并绑定到一个元素
-	 * @param element [HTMLElement] 需要绑定click事件的元素（注意：手动绑定不要使用id="TencentCaptcha"的元素，避免重复绑定点击）
-	 * @param appId [String] 申请的场景Id
-	 * @param callback [Function] 回调函数
-	 * @param options [Object] 更多配置参数, 详见配置参数
+	 * @param element {HTMLElement} 需要绑定click事件的元素（注意：手动绑定不要使用id="SimCaptcha"的元素，避免重复绑定点击）
+	 * @param appId {String} 申请的场景Id
+	 * @param callback {Function} 回调函数
+	 * @param options {Object} 更多配置参数, 详见配置参数
 	 */
 	function SimCaptcha(element, appId, callback, options) {
 		_element = element;
@@ -57,10 +159,22 @@
 	SimCaptcha.prototype = {
 		constructor: SimCaptcha,
 
+		/**
+		 * 设置验证码服务端URL
+		 * @param {String} reqVCodeImgUrl 验证码服务端 图片Url
+		 * @param {String} reqVCodeCheckUrl 验证码服务端 效验Url
+		 */
+		setUrl: function (reqVCodeImgUrl, reqVCodeCheckUrl) {
+			_reqVCodeImgUrl = reqVCodeImgUrl;
+			_reqVCodeCheckUrl = reqVCodeCheckUrl;
+		},
+
 		/***
 		 * 显示验证码
 		 */
 		show: function () {
+			// TODO: 显示验证码弹出层
+			// TODO: 需DOM操作
 		},
 
 		/***
@@ -78,7 +192,53 @@
 		 * return: Object:{"appId":"","ticket":""}
 		 */
 		getTicket: function () {
+			return {
+				appId: _resAppId,
+				ticket: _resTicket
+			};
 		},
+
+
+
+	}
+
+	/**
+	 * 发送http GET
+	 * @param {String} url 请求url
+	 * @param {Function} callback 请求成功回调函数
+	 */
+
+	function httpGet(url, callback) {
+		// XMLHttpRequest对象用于在后台与服务器交换数据   
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', url, true);
+		xhr.onreadystatechange = function () {
+			// readyState == 4说明请求已完成
+			if (xhr.readyState == 4 && xhr.status == 200 || xhr.status == 304) {
+				// 从服务器获得数据 
+				callback.call(this, xhr.responseText);
+			}
+		};
+		xhr.send();
+	}
+
+	/**
+	 * 发送http POST
+	 * @param {String} url 请求url
+	 * @param {Ojbect} data 将要发送的数据包装为对象
+	 * @param {Function} callback 请求成功回调函数
+	 */
+	function httpPost(url, data, callback) {
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", url, true);
+		// 添加http头，发送信息至服务器时内容编码类型
+		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 304)) {
+				callback.call(this, xhr.responseText);
+			}
+		};
+		xhr.send(JSON.stringify(data));
 	}
 
 
