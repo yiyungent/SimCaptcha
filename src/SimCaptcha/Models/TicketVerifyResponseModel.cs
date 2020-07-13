c9c085bb50fb3db5ba9d69437112818da1bf254d
yiyun <yiyungent@126.com>
2020-07-13 15:49:12 +0800

    Updated: ticket无效-5:added 过期被自动删除

--- src/SimCaptcha/Models/TicketVerifyResponseModel.cs
+++ src/SimCaptcha/Models/TicketVerifyResponseModel.cs
@@ -12,7 +12,7 @@ namespace SimCaptcha.Models
         /// <para>-2 verify ticket timeout. 验证码ticket过期</para>
         /// <para>-3 verify ip no match. ip不匹配</para>
         /// <para>-4 decrypt fail. 验证码ticket解密失败</para>
-        /// <para>-5 验证码ticket无效, 可能已被使用过一次</para>
+        /// <para>-5 验证码ticket无效, 可能已被使用过一次以至于被删除,或则过期被自动删除</para>
         /// <para>-6 验证码ticket无效, 篡改ticket, 与验证码服务端保存的此会话ticket不一致</para>
         /// <para>-7 AppId,AppSecret效验不通过</para>
         /// </summary>
