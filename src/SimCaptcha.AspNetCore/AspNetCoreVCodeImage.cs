5ecc9b1e956d58834933f1d9b52a3d9f4ed9f686
yiyun <yiyungent@gmail.com>
2021-02-09 15:31:25 +0800

    feat: use local fonts

--- src/SimCaptcha.AspNetCore/AspNetCoreVCodeImage.cs
+++ src/SimCaptcha.AspNetCore/AspNetCoreVCodeImage.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Linq;
 // Project: SimCaptcha
 // https://github.com/yiyungent/SimCaptcha
 // Author: yiyun <yiyungent@gmail.com>
@@ -20,38 +21,47 @@ namespace SimCaptcha.AspNetCore
             // TODO: 变化点: 答案: 4个字
             int rightCodeLength = 4;
 
-            Bitmap Img = null;
+            Bitmap bitmap = null;
             Graphics g = null;
             MemoryStream ms = null;
             Random random = new Random();
 
-            Color[] color_Array = { Color.Black, Color.DarkBlue, Color.Green, Color.Orange, Color.Brown, Color.DarkCyan, Color.Purple };
-            string[] fonts = { "lnk Free", "Segoe Print", "Comic Sans MS", "MV Boli", "华文行楷" };
-            // TODO: 可能有错, 在windows, linux下 分割符不同
-            string _base = Path.Combine(Environment.CurrentDirectory, "SimCaptcha", "bgImages");
-            //string _base = Environment.CurrentDirectory + "\\SimCaptcha\\bgImages\\";
+            Color[] colorArray = { Color.Black, Color.DarkBlue, Color.Green, Color.Orange, Color.Brown, Color.DarkCyan, Color.Purple };
 
-            var _file_List = System.IO.Directory.GetFiles(_base);
-            int imageCount = _file_List.Length;
-            if (imageCount == 0)
-                throw new Exception("image not Null");
+            string bgImagesDir = Path.Combine(Environment.CurrentDirectory, "SimCaptcha", "bgImages");
+            string[] bgImagesFiles = System.IO.Directory.GetFiles(bgImagesDir);
+            if (bgImagesFiles == null || bgImagesFiles.Length == 0)
+            {
+                throw new Exception("SimCaptcha/bgImages 需放置背景图片");
+            }
 
-            int imageRandom = random.Next(1, (imageCount + 1));
-            string _random_file_image = _file_List[imageRandom - 1];
-            var imageStream = Image.FromFile(_random_file_image);
+            // 字体来自：https://www.zcool.com.cn/special/zcoolfonts/
+            string fontsDir = Path.Combine(Environment.CurrentDirectory, "SimCaptcha", "fonts");
+            string[] fontFiles = new DirectoryInfo(fontsDir)?.GetFiles()
+                ?.Where(m => m.Extension.ToLower() == ".ttf")
+                ?.Select(m => m.FullName).ToArray();
+            if (fontFiles == null || fontFiles.Length == 0)
+            {
+                throw new Exception("SimCaptcha/fonts 需放置字体文件 .ttf");
+            }
 
-            Img = new Bitmap(imageStream, width, height);
+
+            int imgIndex = random.Next(bgImagesFiles.Length);
+            string randomImgFile = bgImagesFiles[imgIndex];
+            var imageStream = Image.FromFile(randomImgFile);
+
+            bitmap = new Bitmap(imageStream, width, height);
             imageStream.Dispose();
-            g = Graphics.FromImage(Img);
+            g = Graphics.FromImage(bitmap);
             Color[] penColor = { Color.LightGray, Color.Green, Color.Blue };
             int code_length = code.Length;
             List<string> words = new List<string>();
             for (int i = 0; i < code_length; i++)
             {
-                int cindex = random.Next(color_Array.Length);
-                int findex = random.Next(fonts.Length);
-                Font f = new Font(fonts[findex], 15, FontStyle.Bold);
-                Brush b = new SolidBrush(color_Array[cindex]);
+                int colorIndex = random.Next(colorArray.Length);
+                int fontIndex = random.Next(fontFiles.Length);
+                Font f = LoadFont(fontFiles[fontIndex], 15, FontStyle.Bold);
+                Brush b = new SolidBrush(colorArray[colorIndex]);
                 int _y = random.Next(height);
                 if (_y > (height - 30))
                     _y = _y - 60;
@@ -79,9 +89,9 @@ namespace SimCaptcha.AspNetCore
             rtnResult.VCodeTip = "请依次点击: " + string.Join(",", words);
 
             ms = new MemoryStream();
-            Img.Save(ms, ImageFormat.Jpeg);
+            bitmap.Save(ms, ImageFormat.Jpeg);
             g.Dispose();
-            Img.Dispose();
+            bitmap.Dispose();
             ms.Dispose();
             rtnResult.ImgBase64 = "data:image/jpg;base64," + Convert.ToBase64String(ms.GetBuffer());
 
@@ -95,7 +105,7 @@ namespace SimCaptcha.AspNetCore
         /// <param name="widthAndHeight">图片宽高</param>
         /// <param name="xAndy">相对于图片的绝对尺寸</param>
         /// <returns>(int:xPercent, int:yPercent)</returns>
-        private (int, int) ToPercentPos((int, int) widthAndHeight, (int, int) xAndy)
+        protected (int, int) ToPercentPos((int, int) widthAndHeight, (int, int) xAndy)
         {
             (int, int) rtnResult = (0, 0);
             // 注意: int / int = int (小数部分会被截断)
@@ -104,5 +114,32 @@ namespace SimCaptcha.AspNetCore
 
             return rtnResult;
         }
+
+
+        /// <summary>
+        /// 加载字体
+        /// </summary>
+        /// <param name="path">字体文件路径,包含字体文件名和后缀名</param>
+        /// <param name="size">大小</param>
+        /// <param name="fontStyle">字形(常规/粗体/斜体/粗斜体)</param>
+        protected Font LoadFont(string path, int size, FontStyle fontStyle)
+        {
+            try
+            {
+                System.Drawing.Text.PrivateFontCollection pfc = new System.Drawing.Text.PrivateFontCollection();
+
+                pfc.AddFontFile(path);// 字体文件的路径
+
+                Font myFont = new Font(pfc.Families[0], size, fontStyle);
+
+                return myFont;
+            }
+            catch (System.Exception)
+            {
+                return null;
+            }
+        }
     }
+
+
 }
---
+++ src/SimCaptcha.AspNetCore/readme.txt
@@ -0,0 +1,7 @@
+﻿SimCaptcha/bgImages 为验证码背景图片文件夹，可以自己替换图片
+
+SimCaptcha/fonts 为验证码字体文件夹，字体文件只支持 xxx.ttf
+
+https://github.com/yiyungent/SimCaptcha
+
+
