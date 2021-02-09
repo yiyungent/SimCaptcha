7797e5c4581a3b7f29bb44740bb1f2f11a84c282
yiyun <yiyungent@gmail.com>
2021-02-09 14:04:43 +0800

    feat: adapt to WebSDK default API

--- src/SimCaptcha.AspNetCore/SimCaptchaMiddlewareExtensions.cs
+++ src/SimCaptcha.AspNetCore/SimCaptchaMiddlewareExtensions.cs
@@ -14,18 +14,18 @@ namespace SimCaptcha.AspNetCore
     {
         public static IApplicationBuilder UseSimCaptcha(this IApplicationBuilder builder)
         {
-            builder.Map("/api/vCode/vCodeImg", app => app.UseMiddleware<VCodeImgMiddleware>());
-            builder.Map("/api/vCode/vCodeCheck", app => app.UseMiddleware<VCodeCheckMiddleware>());
-            builder.Map("/api/vCode/ticketVerify", app => app.UseMiddleware<TicketVerifyMiddleware>());
+            builder.Map("/api/SimCaptcha/Img", app => app.UseMiddleware<VCodeImgMiddleware>());
+            builder.Map("/api/SimCaptcha/Check", app => app.UseMiddleware<VCodeCheckMiddleware>());
+            builder.Map("/api/SimCaptcha/TicketVerify", app => app.UseMiddleware<TicketVerifyMiddleware>());
 
             return builder;
         }
 
         public static IApplicationBuilder UseSimCaptcha(this IApplicationBuilder builder, SimCaptchaOptions options)
         {
-            builder.Map("/api/vCode/vCodeImg", app => app.UseMiddleware<VCodeImgMiddleware>(new OptionsWrapper<SimCaptchaOptions>(options)));
-            builder.Map("/api/vCode/vCodeCheck", app => app.UseMiddleware<VCodeCheckMiddleware>(new OptionsWrapper<SimCaptchaOptions>(options)));
-            builder.Map("/api/vCode/ticketVerify", app => app.UseMiddleware<TicketVerifyMiddleware>(new OptionsWrapper<SimCaptchaOptions>(options)));
+            builder.Map("/api/SimCaptcha/Img", app => app.UseMiddleware<VCodeImgMiddleware>(new OptionsWrapper<SimCaptchaOptions>(options)));
+            builder.Map("/api/SimCaptcha/Check", app => app.UseMiddleware<VCodeCheckMiddleware>(new OptionsWrapper<SimCaptchaOptions>(options)));
+            builder.Map("/api/SimCaptcha/TicketVerify", app => app.UseMiddleware<TicketVerifyMiddleware>(new OptionsWrapper<SimCaptchaOptions>(options)));
 
             return builder;
         }
